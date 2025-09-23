f032ef2258fc559dfaf00e3d40972b1530a879a2
krematorial <137116006+krematorial@users.noreply.github.com>
2025-09-23 21:00:32 +0300

    Add files via upload

---
+++ netkliks-angular 2/src/app/app-module.ts
@@ -0,0 +1,29 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Card } from './components/card/card';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Search } from './components/search/search';
+
+@NgModule({
+  declarations: [
+    App,
+    Card,
+    Header,
+    Sidebar,
+    Search
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
